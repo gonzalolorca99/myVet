804bfb6017ea799e020ca4593dee79fb416cf763
Pablo López Vílchez <pablolv29@gmail.com>
2021-05-17 11:53:07 +0200

    Fragment aboutUS finiquitao

--- app/src/main/java/com/dam/myvet/ui/aboutAs/aboutAsFragment.java
+++ app/src/main/java/com/dam/myvet/ui/aboutAs/aboutAsFragment.java
@@ -1,9 +1,12 @@
 package com.dam.myvet.ui.aboutAs;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,22 +20,35 @@ import com.dam.myvet.R;
 public class aboutAsFragment extends Fragment {
 
     private aboutAsViewModel aboutAsViewModel;
+    private Button btdatos;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        aboutAsViewModel =
-                new ViewModelProvider(this).get(aboutAsViewModel.class);
 
-        View root = inflater.inflate(R.layout.fragment_inicioc, container, false);
+        View root = inflater.inflate(R.layout.fragment_aboutas, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
-        aboutAsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        btdatos = (Button) root.findViewById(R.id.btprivdatos);
+        btdatos.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext())
+                        .setTitle("Información de privacidad")
+                        .setMessage("MyVet no recopila ningún tipo de información de usuario ni de uso." + "\n" +
+                                "\nSolamente se almacenan en Firebase los datos de autenticación e información relevante para el correcto funcionamiento de la app")
+
+                        .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+                alert.show();
+
             }
         });
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
